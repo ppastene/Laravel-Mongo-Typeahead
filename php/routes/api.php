6003d198eb80b5d8b42528d124a64114b4de6973
Pedro Pablo Pastene <pe.pastene@gmail.com>
2020-08-05 03:38:12 -0400

    Song API Routes

--- php/routes/api.php
+++ php/routes/api.php
@@ -14,6 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+/*
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+*/
+
+Route::group(['prefix' => 'songs'], function() {
+    Route::get('/', 'SongController@index');
+    Route::get('/{request}', 'SongController@search');
 });
